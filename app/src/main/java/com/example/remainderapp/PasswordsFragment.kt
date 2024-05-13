24cfa22f6a08a4ed599515b8adcc5028394248a2
mr_inceptionist <self.dipak@gmail.com>
2024-05-14 00:26:43 +0530

    Added viewpager and tablayout

---
+++ app/src/main/java/com/example/remainderapp/PasswordsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.remainderapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.remainderapp.databinding.FragmentPasswordsBinding
+
+class PasswordsFragment:Fragment() {
+    private lateinit var binding:FragmentPasswordsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding=FragmentPasswordsBinding.inflate(inflater,container,false)
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
